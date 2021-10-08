9e1a045a8424c347f447076d3ca346a06ebe7d90
Elyse Roth <elysemorganroth@gmail.com>
2021-10-08 09:12:56 -0400

    Add project files.

---
+++ SalesDbLib/Models/AppDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesDbLib.Models
+{
+    public class AppDbContext : DbContext
+    {
+        //DbSets go here (once create classes)
+        public virtual DbSet<Customer> Customers { get; set; } //if forget to put class in here you will notice (DbSet is generic class, used for EF)
+            
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Customer>(e => //have to specify which entity (all things using fluid API will use this phrasing)
+            {
+                e.HasIndex(p => p.Code).IsUnique(true); //first part is operation only, second part tells sql that column is unique
+                e.Property(p => p.Name).HasMaxLength(30).IsRequired(true); //Fluid API version of max length and not null instead of attributes
+            
+            });
+        }
+
+        public AppDbContext() { }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) 
+            : base(options) {}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            if(!builder.IsConfigured)
+            {
+                var connStr = "server=localhost\\sqlexpress;database=EfSalesDb;trusted_connection=true;"; //same conn string, diff DB
+                builder.UseSqlServer(connStr);
+            }
+        }
+    }
+}
---
+++ SalesDbLib/Models/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesDbLib.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        [StringLength(30)] //use attribute to designate max length, unique, attributes go in []
+        [Required] //will force string column to be not null in db
+        public string Code { get; set; }
+        [StringLength(30), Required] //can also do multiple attributes in one [] to achieve same goal as above with code
+        public string Name { get; set; }
+        [Column(TypeName = "decimal(11,2)") ] //type sql uses for column
+        public decimal Sales { get; set; }
+        public DateTime Created { get; set; }
+
+        public Customer() { }
+
+
+    }
+}
